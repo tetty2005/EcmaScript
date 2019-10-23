c2c36a3f1d90abe1e54ca2b8301e6a5df6be815e
Tetiana Lebid <t.lebed17@gmail.com>
2019-10-23 18:35:35 +0300

    first

---
+++ 1_let_const.js
@@ -0,0 +1,16 @@
+// if (true) {
+//     var v = 'var'; // var will be visible out of {}
+//     let l = 'let'; // var will be visible only inside {}
+// }
+// console.log(v);
+// console.log(l);
+
+// Hoisting
+// b = 20;
+// console.log(b); // к переменной var можно обращаться еще до ее объявления
+// var b = 10;
+//
+// l = 20;
+// console.log(l); // переменные let не хостятся, к ним нельзя обращаться до объявления
+// let l = 10;
+
